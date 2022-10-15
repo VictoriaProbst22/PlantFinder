605462dbc3ca2bb93387cadab92a79b71212047d
Victoria Probst <victoria.probst13@gmail.com>
2022-10-15 14:51:24 -0400

    Updated Views.py File to accept GET and POST requests for Recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Recipe
+from .serializers import RecipeSerializer
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def recipes_list(request):
-
-
-
-    return Response('ok')
+    if request.method == 'GET':
+        recipes = Recipe.objects.all()
+        serializer = RecipeSerializer(recipes, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = RecipeSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
